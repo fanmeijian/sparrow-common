dc393716e6e70edd6bb2b11fcd5b2ce258b6c699
Fan Sword <fanmeijian@gmail.com>
2025-08-18 00:05:26 +0800

    完成页面元素的功能，增加个人信息保存

--- src/main/java/cn/sparrowmini/common/CurrentUser.java
+++ src/main/java/cn/sparrowmini/common/CurrentUser.java
@@ -4,16 +4,23 @@ public class CurrentUser {
 //	public static final CurrentUser INSTANCE = new CurrentUser();
 
 	private static final ThreadLocal<String> storage = new ThreadLocal<>();
+	private static final ThreadLocal<UserInfo> userInfo_ = new ThreadLocal<>();
 
-	public static void logIn(String user) {
-		storage.set(user);
+	public static void logIn(UserInfo userInfo) {
+		storage.set(userInfo.getUsername());
+		userInfo_.set(userInfo);
 	}
 
 	public static void logOut() {
 		storage.remove();
+		userInfo_.remove();
 	}
 
 	public static String get() {
 		return storage.get();
+	}
+
+	public static UserInfo getUserInfo(){
+		return userInfo_.get();
 	}
 }
