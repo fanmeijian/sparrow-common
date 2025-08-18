d1845bb4eadbc3144ecde596a3b65041fc7a023f
Fan Sword <fanmeijian@gmail.com>
2025-08-18 17:10:40 +0800

    增加树的dto，方法，但还不完善

--- src/main/java/cn/sparrowmini/common/dto/BaseTreeDto.java
+++ src/main/java/cn/sparrowmini/common/dto/BaseTreeDto.java
@@ -1,6 +1,10 @@
 package cn.sparrowmini.common.dto;
 
 import cn.sparrowmini.common.model.BaseTree;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.Value;
 
 import java.io.Serializable;
@@ -9,7 +13,7 @@ import java.math.BigDecimal;
 /**
  * DTO for {@link BaseTree}
  */
-@Value
+@Getter
 public class BaseTreeDto implements Serializable {
     String id;
     String parentId;
@@ -19,4 +23,19 @@ public class BaseTreeDto implements Serializable {
     Boolean includeAllChildren;
     String description;
     BigDecimal seq;
+
+    @Setter
+    @Transient
+    long childCount=0;
+
+    public BaseTreeDto(String id, String parentId, String name, String code, String catalogId, Boolean includeAllChildren, String description, BigDecimal seq) {
+        this.id = id;
+        this.parentId = parentId;
+        this.name = name;
+        this.code = code;
+        this.catalogId = catalogId;
+        this.includeAllChildren = includeAllChildren;
+        this.description = description;
+        this.seq = seq;
+    }
 }
